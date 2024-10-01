afb05758820f8cd94f0ae448949e067d9629b052
epiland <pilandethan@gmail.com>
2024-10-01 12:29:33 -0400

    Feat: Started on API

---
+++ app/src/api/endpoints/DuoAuth.kt
@@ -0,0 +1,7 @@
+package api.endpoints
+
+object DuoAuth {
+    const val DUOAUTH_INTEGRATION_KEY = "DI2O0DZMTPJVIFEC2JRX"
+    const val DUOAUTH_SECRET_KEY = "f9JlxYgvFi57gOUec1ujFFGX4ypDJteSqnp0Xd27"
+    const val DUOAUTH_API_HOSTNAME = "api-06d4618e.duosecurity.com"
+}
