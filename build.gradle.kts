f9ed3b315bc5c1c748d9a395d9ada650d9074efd
Royal-Blue <wuilmervelasquez.93@gmail.com>
2024-10-09 10:55:29 -0400

    implemented Firebase Realtime Database and authentication to store user info.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,20 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.google.gms:google-services:4.4.2")
+    }
+}
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.google.gms.google.services) apply false
+
 }
+
+
